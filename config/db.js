322705376ff8184e0ff1a72d1fa9b670c200fddc
Canen <Alvaro@Alvaros-MacBook-Pro.local>
2014-11-20 16:20:37 -0800

    dashboard and history views

--- config/db.js
+++ config/db.js
@@ -1,24 +1,24 @@
 var db = require('mysql');
 
-//pc
-var connection = db.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : '',
-        database: 'cd_login',
-        multipleStatements: true
-});
+// var connection = db.createConnection({
+//         host : 'localhost',
+//         user : 'root',
+//         password : '',
+//        //pc
+//  database: 'cd_login',
+//         multipleStatements: true
+// });
 
 
 //mac
-// var connection = db.createConnection({
-//              host : 'localhost',
-//              user : 'root',
-//          password : 'root',
-//          database : 'clock_n_clock_out',
-// multipleStatements: true
-//              port : 8889
-// });
+var connection = db.createConnection({
+             host : 'localhost',
+             user : 'root',
+         password : 'root',
+         database : 'cd_login',
+multipleStatements: true,
+             port : 8889
+});
 
 
 module.exports = db;
