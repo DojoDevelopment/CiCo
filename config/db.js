6d918f6bb190b68431fd8c09a870a371f6a48a2b
antfenech <antfenech@yahoo.com>
2014-11-20 22:28:27 -0800

    I don't know what to do with my hands

--- config/db.js
+++ config/db.js
@@ -1,43 +1,23 @@
 var db = require('mysql');
 
-<<<<<<< HEAD
-//pc
-// var connection = db.createConnection({
-// 			        host : 'localhost',
-// 			        user : 'root',
-// 	        password : '',
-// 					 database: 'cd_login',
-//  multipleStatements: true
-=======
 
 //pc
-
-// var connection = db.createConnection({
-//         host : 'localhost',
-//         user : 'root',
-//         password : '',
-//         database: 'cd_login',
-
-//        //pc
-//         multipleStatements: true
->>>>>>> 3088c86de226f67752249cebd739a8d1030f83b5
-// });
-
-
-//mac
 var connection = db.createConnection({
-             host : 'localhost',
-             user : 'root',
-         password : 'root',
-         database : 'cd_login',
-multipleStatements: true,
-             port : 8889
+			        host : 'localhost',
+			        user : 'root',
+	        password : '',
+					 database: 'cd_login',
+ multipleStatements: true
 });
+//mac
+// var connection = db.createConnection({
+//              host : 'localhost',
+//              user : 'root',
+//          password : 'root',
+//          database : 'cd_login',
+// multipleStatements: true,
+//              port : 8889
+// });
 
 module.exports = db;
 module.exports = connection;
-
-
-
-
-
