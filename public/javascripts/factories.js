a9515528efb603097d233a11aece7eaabf466024
antfenech <antfenech@yahoo.com>
2014-11-22 15:50:22 -0800

    custom css

--- public/javascripts/factories.js
+++ public/javascripts/factories.js
@@ -14,3 +14,23 @@ app.factory('DashboardFactory', function($http){
 
 	};
 });
+
+app.factory('HistoryFactory', function($http){
+	return {
+		get_locations: function(callback){
+			$http.get('/api/get_locations').success(function(data){
+				callback(data);
+			});
+		},
+		get_members: function(callback){
+			$http.get('api/get_members').success(function(data){
+				callback(data);
+			});
+		},
+		get_history_table : function(callback){
+			$http.get('/api/get_hist_table').success(function(data){
+				callback(data);
+			});
+		}
+	};
+});
