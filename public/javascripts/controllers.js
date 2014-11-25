273cfdb0b00724129c34cf967b9fe5b44665777e
antfenech <antfenech@yahoo.com>
2014-11-25 15:04:45 -0800

    location filter in admin history

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -96,74 +96,72 @@ app.controller('history', function($scope, TableFactory, ListFactory) {
 		$scope.order = '-date';
 	});        
 
-});	
-    
-    $scope.dateFilter = function(date_range) {
-    	
-    	if ( ( typeof(date_range) != 'string' ) && (date_range[0] < date_range[1]) ) {
-    		//console.log(date_range);
-    		start_date = new Date(date_range[0]);
-    		end_date = new Date(date_range[1]);
-    		//console.log(start_date,end_date);
-    		
-    		TableFactory.get_factory_history_table(function(data){
-    			$scope.table = data;
-    			var table2=[]
-    			for (var i = 0; i < $scope.table.length - 1; i++) {
-    				work_session_date = new Date($scope.table[i].created_at);
-    				if (work_session_date > start_date && work_session_date < end_date){
-    					table2.push($scope.table[i]);
-    				}
-    			}
-    			$scope.table=table2;
-    		})
-    	}
-    	else if(date_range === 'all') {
-            TableFactory.get_factory_history_table(function(data){
-				$scope.table = data;
-				$scope.order = '-date';
-        	})
-        }	
-        else if(date_range === 'this_week') {
-            var table2=[];
-            for (var i = 0; i < $scope.table.length - 1; i++) {
-            	work_session_date = new Date($scope.table[i].created_at);
-            	if (work_session_date > Date.now() - 7*24*3600*1000){
-            		table2.push($scope.table[i]);
-            	};         
-            }
-            $scope.table=table2; 
-        }
-        else if (date_range === 'last_week') {
-            var table2=[];
-            for (var i = 0; i < $scope.table.length - 1; i++) {
-            	work_session_date = new Date($scope.table[i].created_at);
-            	if (work_session_date < (Date.now() - 7*24*3600*1000) && work_session_date > (Date.now() - 14*24*3600*1000) ){
-            		table2.push($scope.table[i]);
-            	};         
-            }
-            $scope.table=table2;
-        }
-        else if (date_range === 'this_month') {
-            var table2=[];
-            for (var i = 0; i < $scope.table.length - 1; i++) {
-            	work_session_date = new Date($scope.table[i].created_at);
-            	if ( work_session_date > (Date.now() - 30*24*3600*1000) ){
-            		table2.push($scope.table[i]);
-            	};         
-            }
-            $scope.table=table2;
-        }
-         else if (date_range === 'last_month') {
-            var table2=[];
-            for (var i = 0; i < $scope.table.length - 1; i++) {
-            	work_session_date = new Date($scope.table[i].created_at);
-            	if (work_session_date < (Date.now() - 30*24*3600*1000) && work_session_date > (Date.now() - 60*24*3600*1000) ){
-            		table2.push($scope.table[i]);
-            	};         
-            }
-            $scope.table=table2;
-        }
-   
+  $scope.dateFilter = function(date_range) {
+  	
+  	if ( ( typeof(date_range) != 'string' ) && (date_range[0] < date_range[1]) ) {
+  		//console.log(date_range);
+  		start_date = new Date(date_range[0]);
+  		end_date = new Date(date_range[1]);
+  		//console.log(start_date,end_date);
+  		
+  		TableFactory.get_factory_history_table(function(data){
+  			$scope.table = data;
+  			var table2=[]
+  			for (var i = 0; i < $scope.table.length - 1; i++) {
+  				work_session_date = new Date($scope.table[i].created_at);
+  				if (work_session_date > start_date && work_session_date < end_date){
+  					table2.push($scope.table[i]);
+  				}
+  			}
+  			$scope.table=table2;
+  		})
+  	}
+  	else if(date_range === 'all') {
+          TableFactory.get_factory_history_table(function(data){
+			$scope.table = data;
+			$scope.order = '-date';
+      	})
+      }	
+      else if(date_range === 'this_week') {
+          var table2=[];
+          for (var i = 0; i < $scope.table.length - 1; i++) {
+          	work_session_date = new Date($scope.table[i].created_at);
+          	if (work_session_date > Date.now() - 7*24*3600*1000){
+          		table2.push($scope.table[i]);
+          	};         
+          }
+          $scope.table=table2; 
+      }
+      else if (date_range === 'last_week') {
+          var table2=[];
+          for (var i = 0; i < $scope.table.length - 1; i++) {
+          	work_session_date = new Date($scope.table[i].created_at);
+          	if (work_session_date < (Date.now() - 7*24*3600*1000) && work_session_date > (Date.now() - 14*24*3600*1000) ){
+          		table2.push($scope.table[i]);
+          	};         
+          }
+          $scope.table=table2;
+      }
+      else if (date_range === 'this_month') {
+          var table2=[];
+          for (var i = 0; i < $scope.table.length - 1; i++) {
+          	work_session_date = new Date($scope.table[i].created_at);
+          	if ( work_session_date > (Date.now() - 30*24*3600*1000) ){
+          		table2.push($scope.table[i]);
+          	};         
+          }
+          $scope.table=table2;
+      }
+       else if (date_range === 'last_month') {
+          var table2=[];
+          for (var i = 0; i < $scope.table.length - 1; i++) {
+          	work_session_date = new Date($scope.table[i].created_at);
+          	if (work_session_date < (Date.now() - 30*24*3600*1000) && work_session_date > (Date.now() - 60*24*3600*1000) ){
+          		table2.push($scope.table[i]);
+          	};         
+          }
+          $scope.table=table2;
+      }
+ 
     } //end of $scope.dateFilter function
 }); //end of history controller
