ad6ba7b098980ab071630f86274fb98cf8006720
Canen <Alvaro@Alvaros-MacBook-Pro.local>
2014-11-24 17:52:16 -0800

    time filters almost done

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -50,5 +50,57 @@ app.controller('history', function($scope, TableFactory, ListFactory) {
 		$scope.table = data;
 		$scope.order = '-date';
 	});
+        
+    
+    $scope.dateFilter = function(date_range) {
+    	if(date_range === 'all') {
+            TableFactory.get_factory_history_table(function(data){
+			$scope.table = data;
+			$scope.order = '-date';
+        	})
+        }	
+        else if(date_range === 'this_week') {
+            var table2=[];
+            for (var i = 0; i < $scope.table.length - 1; i++) {
+            	work_session_date = new Date($scope.table[i].created_at);
+            	if (work_session_date > Date.now() - 7*24*3600*1000){
+            		table2.push($scope.table[i]);
+            	};         
+            }
+            $scope.table=table2; 
+        }
+        else if (date_range === 'last_week') {
+            var table2=[];
+            for (var i = 0; i < $scope.table.length - 1; i++) {
+            	work_session_date = new Date($scope.table[i].created_at);
+            	if (work_session_date < (Date.now() - 7*24*3600*1000) && work_session_date > (Date.now() - 14*24*3600*1000) ){
+            		table2.push($scope.table[i]);
+            	};         
+            }
+            $scope.table=table2;
+        }
+        else if (date_range === 'this_month') {
+            var table2=[];
+            for (var i = 0; i < $scope.table.length - 1; i++) {
+            	work_session_date = new Date($scope.table[i].created_at);
+            	if ( work_session_date > (Date.now() - 30*24*3600*1000) ){
+            		table2.push($scope.table[i]);
+            	};         
+            }
+            $scope.table=table2;
+        }
+         else if (date_range === 'last_month') {
+            var table2=[];
+            for (var i = 0; i < $scope.table.length - 1; i++) {
+            	work_session_date = new Date($scope.table[i].created_at);
+            	if (work_session_date < (Date.now() - 30*24*3600*1000) && work_session_date > (Date.now() - 60*24*3600*1000) ){
+            		table2.push($scope.table[i]);
+            	};         
+            }
+            $scope.table=table2;
+        }
+
+        
+    }
 	
 });
