1316096a001d86bfadb5db6ed7f58c239c944a11
Canen <Alvaro@Alvaros-MacBook-Pro.local>
2014-12-02 16:15:03 -0800

    new sql script and db.js connection changed for heroku

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
-var conString = "postgres://postgres:password@localhost/cico";
+//var conString = "postgres://postgres:password@localhost/cico";
+var conString = "postgres://eshqknkxtopmed:S4Z1PsaKF_O5gGcyvi9z70zTD5@ec2-23-23-183-5.compute-1.amazonaws.com:5432/d4th0jqd2a0s30";
 module.exports = conString;
 //sever:localhost
 //database: cico
