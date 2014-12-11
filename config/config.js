e9279764e0e66c581d7de335321b4ac25b3daae7
Canen <Alvaro@Alvaros-MacBook-Pro.local>
2014-12-11 13:15:39 -0800

    heroku postgres fix

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
-var path = require('path')
-  , rootPath = path.normalize(__dirname + '/..')
-  , templatePath = path.normalize(__dirname + '/../server/mailer/templates')
-  , notifier = {
-      service: 'postmark',
-      APN: false,
-      email: false, // true
-      actions: ['comment'],
-      tplPath: templatePath,
-      key: 'POSTMARK_KEY',
-      parseAppId: 'PARSE_APP_ID',
-      parseApiKey: 'PARSE_MASTER_KEY'
-    }
+// var path = require('path')
+//   , rootPath = path.normalize(__dirname + '/..')
+//   , templatePath = path.normalize(__dirname + '/../server/mailer/templates')
+//   , notifier = {
+//       service: 'postmark',
+//       APN: false,
+//       email: false, // true
+//       actions: ['comment'],
+//       tplPath: templatePath,
+//       key: 'POSTMARK_KEY',
+//       parseAppId: 'PARSE_APP_ID',
+//       parseApiKey: 'PARSE_MASTER_KEY'
+//     }
